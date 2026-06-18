c45b3174737c012749cfd6a431e2c765f7d44dba
Devansh <deevansh346@gmail.com>
2026-06-18 22:22:37 +0530

    Update

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from src.components.dialog_auto_enroll import auto_enroll_dialog
 def main():
 
     st.set_page_config(
-        page_title='SnapClass - Making Attendance faster using AI',
-        page_icon="https://i.ibb.co/YTYGn5qV/logo.png"
+        page_title='FaceTrack AI - Making Attendance faster using AI',
+        page_icon="https://chatgpt.com/backend-api/estuary/content?id=file_000000009e8471fb82074a2e856a3220&ts=494944&p=fs&cid=1&sig=750ac3fe2904f8439d97f12ceff6ce4790a12d34877006448d0d793ba0b91de0&v=0"
     )
     
     if 'login_type' not in st.session_state:
