08526c2e22cd89c72a6912a2294c8ac0f29d0138
Devansh <deevansh346@gmail.com>
2026-06-18 22:36:13 +0530

    Update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
 
     st.set_page_config(
         page_title='FaceTrack AI - Making Attendance faster using AI',
-        page_icon="https://chatgpt.com/backend-api/estuary/content?id=file_000000009e8471fb82074a2e856a3220&ts=494944&p=fs&cid=1&sig=750ac3fe2904f8439d97f12ceff6ce4790a12d34877006448d0d793ba0b91de0&v=0"
+        page_icon="https://www.dreamstime.com/digital-human-face-rendered-network-connected-blue-dots-dark-background-image452962244"
     )
     
     if 'login_type' not in st.session_state:
